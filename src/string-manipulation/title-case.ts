eac0575a68f1e1daf07459ac9ea93976c737c89f
Caleb Courier <caleb.courier@gmail.com>
2022-08-16 15:13:30 -0500

    tsdocs

--- src/string-manipulation/title-case.ts
+++ src/string-manipulation/title-case.ts
@@ -1,6 +1,12 @@
 import upperFirst from 'lodash.upperfirst';
 import camelCase from 'lodash.camelcase';
 
+/**
+ * Takes a string and converts it to TitleCase
+ * @param input - string
+ * @returns string
+ * @defaultValue ''
+ */
 function titleCase (input = '') {
   return upperFirst(camelCase(input));
 }
