ea8960a09951374364e38002a35f5dbfc562c9b7
Caleb Courier <caleb.courier@gmail.com>
2022-09-27 15:04:01 -0500

    replace special chars and filter our double hyphens

--- src/string-manipulation/kebab-case.ts
+++ src/string-manipulation/kebab-case.ts
@@ -7,7 +7,21 @@
 function kebabCase (input = '') {
   const snakeReplaced = input.replace(/_/g, '-');
   const camelReplaced = snakeReplaced.replace(/([a-z0-9])([A-Z])/g, '$1-$2');
-  return camelReplaced.toLowerCase();
+  const specialCharsReplaced = camelReplaced.replace(/[^a-zA-Z0-9]/g,'-');
+  const characters = specialCharsReplaced.split('');
+  const doubleHyphenFiltered = characters.reduce((accumultor: string, char: string, index: number) => {
+    if (char === '-') {
+      const previousIndex = index - 1;
+      const previousChar = characters[previousIndex];
+      if (previousChar && previousChar !== '-') {
+        accumultor += char;
+      }
+    } else {
+      accumultor += char;
+    }
+    return accumultor;
+  }, '');
+  return doubleHyphenFiltered.toLowerCase();
 }
 
 export {
