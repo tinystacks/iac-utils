4114908102c5068e6536b98fa9dba0d8516d4952
Caleb Courier <caleb.courier@gmail.com>
2023-01-12 13:09:19 -0600

    re-add coverage

--- jest.config.js
+++ jest.config.js
@@ -3,13 +3,13 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['.d.ts', '.js'],
-  verbose: true
-  // coverageThreshold: {
-  //   global: {
-  //     branches: 75,
-  //     functions: 92,
-  //     lines: 92,
-  //     statements: 92
-  //   }
-  // }
+  verbose: true,
+  coverageThreshold: {
+    global: {
+      branches: 75,
+      functions: 89,
+      lines: 92,
+      statements: 92
+  }
+}
 };
